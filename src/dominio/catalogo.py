cd4dafc5fa10a33e4013954ac4f336bf239ad05e
Sebas_AF <sebasbrownmuller@gmail.com>
2026-09-11 12:46:13 -0300

    Create catalogo.py
    catalogo de canciones de los 2000

---
+++ src/dominio/catalogo.py
@@ -0,0 +1,62 @@
+CATALOGO_POP_2000S = [
+    {
+        "id": 1,
+        "titulo": "Toxic",
+        "artista": "Britney Spears",
+        "anio": 2003,
+        "duracion": "3:18"
+    },
+    {
+        "id": 2,
+        "titulo": "Bad Romance",
+        "artista": "Lady Gaga",
+        "anio": 2009,
+        "duracion": "4:54"
+    },
+    {
+        "id": 3,
+        "titulo": "Hips Don't Lie",
+        "artista": "Shakira feat. Wyclef Jean",
+        "anio": 2006,
+        "duracion": "3:38"
+    },
+    {
+        "id": 4,
+        "titulo": "Umbrella",
+        "artista": "Rihanna feat. JAY-Z",
+        "anio": 2007,
+        "duracion": "4:35"
+    },
+    {
+        "id": 5,
+        "titulo": "Can't Get You Out of My Head",
+        "artista": "Kylie Minogue",
+        "anio": 2001,
+        "duracion": "3:50"
+    },
+    {
+        "id": 6,
+        "titulo": "Bye Bye Bye",
+        "artista": "*NSYNC",
+        "anio": 2000,
+        "duracion": "3:19"
+    },
+    {
+        "id": 7,
+        "titulo": "Crazy in Love",
+        "artista": "Beyoncé feat. JAY-Z",
+        "anio": 2003,
+        "duracion": "3:56"
+    },
+    {
+        "id": 8,
+        "titulo": "Poker Face",
+        "artista": "Lady Gaga",
+        "anio": 2008,
+        "duracion": "3:57"
+    }
+]
+
+
+def obtener_catalogo():
+    return CATALOGO_POP_2000S
