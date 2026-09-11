7e897394bc58383705624d39db760a37201f9771
Sebas_AF <sebasbrownmuller@gmail.com>
2026-09-11 12:55:34 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from src.config import TEMA
 
+from dominio.catalogo import obtener_catalogo
+
 TEMAS = {
     "pokedex": "Pokédex",
     "recetario": "Recetario",
@@ -38,7 +40,9 @@ def main():
         opcion = input("> ").strip()
         if opcion == "0":
             print("Chau.")
-        elif opcion in {"1", "2", "3", "4", "5", "6", "7", "8", "9"}:
+        elif opcion == "1":
+            listar_catalogo()
+        elif opcion in {"2", "3", "4", "5", "6", "7", "8", "9"}:
             pendiente()
         else:
             print("Opción inválida.")
